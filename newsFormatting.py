a6117becf5487dcc56e7a7766bfcec4957d523b6
RitishKarmakar <144108603+RitishKarmakar@users.noreply.github.com>
2024-04-18 17:09:27 +0530

    Modification commit

--- newsFormatting.py
+++
@@ -1,5 +0,0 @@
-from newsextractor import GoogleNews
-gn = GoogleNews()
-
-search_res = gn.search('vote')
-print(search_res)
--- newsapi__init__.py
+++ newsapi__init__.py
@@ -4,18 +4,18 @@ import json
 rapi = 'https://newsapi.org/v2/top-headlines?country=in&apiKey={}'.format('be8c1b00427341bbb654de6635fae2b8')
 #qapi = 'https://newsapi.org/v2/top-headlines?country=in&apiKey={}'.format('pass')
 local_area = 'greater noida'
-category = 'crime'
+category = 'homicide'
 
 req = requests.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=be8c1b00427341bbb654de6635fae2b8')
-print(req.content)
-# newsapi = NewsApiClient('be8c1b00427341bbb654de6635fae2b8')
+#print(req.content)
+newsapi = NewsApiClient('be8c1b00427341bbb654de6635fae2b8')
 # print()
 
 
-# top_headlines = newsapi.get_top_headlines(q=local_area,
-#                                           category=category,
-#                                           language='en',
-#                                           country='in')
+top_headlines = newsapi.get_top_headlines(
+                                          category=category,
+                                          language='en',
+                                          country='in')
 
 
 # all_articles = newsapi.get_everything(q='robery',
@@ -27,3 +27,4 @@ print(req.content)
 
 # print(all_articles)
 sources = newsapi.get_sources()
+print(top_headlines)
